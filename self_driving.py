e63ea9f7a07d8f113be5c0250148860eb454c4ac
Panda <panda@iasa20ui-MacBookPro.local>
2020-11-04 14:04:53 +0900

    constant

--- self_driving.py
+++ self_driving.py
@@ -4,13 +4,13 @@ import numpy as np
 from picamera import PiCamera
 from motor_control import Go, Turn
 
-wheel_base = 700 #앞바퀴 중심 ~ 뒷바퀴 중심
-wheel_width = 670 #뒷바퀴 간격
+Wheel_base = 700 #앞바퀴 중심 ~ 뒷바퀴 중심
+Wheel_width = 670 #뒷바퀴 간격
 
 def get_line(camera):
 
 
 def follow_line(speed):
     camera = PiCamera()
-    
+
 
