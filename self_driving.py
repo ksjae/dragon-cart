db2b9286e25c9d406cf3d72905fc149ee3372e41
Panda <panda@iasa20ui-MacBookPro.local>
2020-11-18 13:31:59 +0900

    ..

--- self_driving.py
+++ self_driving.py
@@ -3,14 +3,16 @@ import pickle
 import numpy as np
 from picamera import PiCamera
 from motor_control import Go, Turn
+from time import time
 
 Wheel_base = 700 #앞바퀴 중심 ~ 뒷바퀴 중심
 Wheel_width = 670 #뒷바퀴 간격
 
-def get_line(camera):
+def get_line(camera,fps):
 
 
-def follow_line(speed):
+def follow_line(speed,fps):
     camera = PiCamera()
+    
 
 
