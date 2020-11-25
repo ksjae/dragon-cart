bcb08fdbb66516a371c8595eb80ada4e3a46071a
Kim HyunMin <paisano55@naver.com>
2020-11-25 14:02:39 +0900

    gpio setup

--- motor_control.py
+++ motor_control.py
@@ -1,4 +1,20 @@
-#GPIU
+import RPi.GPIO as GPIO #run code as admin
+ 
+GPIO.setmode(GPIO.BOARD) #GPIO.BCM : Pin Name. .BOARD : Pin Number by Order 
+#GPIO.setmode(GPIO.BCM)
+
+pwm = 10 #change pinnumber when use RPI. This is according to Arduino sample
+in1 = 11
+in2 = 12
+vcc = 13 
+
+GPIO.setup(pwm, GPIO.OUT)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+GPIO.setup(vcc, GPIO.OUT) 
+GPIO.output(vcc, GPIO.HIGH)
+
+
 
 def go(speed):
     pass
--- self_driving.py
+++ self_driving.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import pickle
 import numpy as np
 #from picamera import PiCamera
-from motor_control import go, turn
+#from motor_control import go, turn
 from time import time
 
 Wheel_base = 700 #앞바퀴 중심 ~ 뒷바퀴 중심
@@ -71,7 +71,8 @@ def videoLanes(camera):
 
 def follow_line(speed,fps):
     #camera = PiCamera()
-    camera = "./test_image/test_vid.MOV"
+    #camera = "./test_image/test_vid.MOV"
+    camera = 0
     videoLanes(camera)
 
 if __name__ == '__main__':
