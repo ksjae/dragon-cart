8d927c2e0d812736bedcb9099ed76ccd192c654a
Kim HyunMin <paisano55@naver.com>
2020-11-27 11:09:43 +0900

    gpio add

--- motor_control.py
+++ motor_control.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO #run code as admin
+import time
  
 GPIO.setmode(GPIO.BOARD) #GPIO.BCM : Pin Name. .BOARD : Pin Number by Order 
 #GPIO.setmode(GPIO.BCM)
@@ -8,17 +9,32 @@ in1 = 11
 in2 = 12
 vcc = 13 
 
+#pin mode setup
 GPIO.setup(pwm, GPIO.OUT)
 GPIO.setup(in1, GPIO.OUT)
 GPIO.setup(in2, GPIO.OUT)
 GPIO.setup(vcc, GPIO.OUT) 
+
+#digital write vcc high
 GPIO.output(vcc, GPIO.HIGH)
 
 
+def go(speed, time, forward):
+    pwmPin = GPIO.PWM(pwm,1000)
+    if(forward):
+        GPIO.output(in1, GPIO.HIGH)
+        GPIO.output(in2, GPIO.LOW)
+    else:
+        GPIO.output(in1, GPIO.LOW)
+        GPIO.output(in2, GPIO.HIGH)
 
-def go(speed):
-    pass
+    
     #code
+
+def stop():
+    GPIO.output(in1, GPIO.LOW)
+    GPIO.output(in2, GPIO.LOW)
+    
 
 def turn(degree):
     pass
